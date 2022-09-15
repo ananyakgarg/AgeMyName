6f938a9dd882f50ec6158e3a3a97bb6daafd834e
Ananya Garg <akgarg@unc.edu>
2022-09-15 15:59:20 -0400

    Initial commit

---
+++ AgeMyName/AgeMyName/AgeMyNameApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgeMyNameApp.swift
+//  AgeMyName
+//
+//  Created by Ananya Garg on 9/15/22.
+//
+
+import SwiftUI
+
+@main
+struct AgeMyNameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
